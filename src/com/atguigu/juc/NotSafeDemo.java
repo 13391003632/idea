9980b5a2c6b747081aab075eb8b4e2df7ecd93cd
qijinxi <834840180@qq.com>
2019-10-11 21:20:53 +0800

    idea第一次修改

--- src/com/atguigu/juc/NotSafeDemo.java
+++ src/com/atguigu/juc/NotSafeDemo.java
@@ -1,7 +1,6 @@
 package com.atguigu.juc;
 
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
@@ -10,7 +9,10 @@ import java.util.concurrent.CopyOnWriteArrayList;
  **/
 public class NotSafeDemo {
     public static void main(String[] args) {
-        List<String> list = new CopyOnWriteArrayList<>();
+       // List<String> list = Collections.synchronizedList(new ArrayList<>());
+        //List<String> list = new Vector<>();
+        List<String> list=new CopyOnWriteArrayList<>();
+
         for (int i = 1; i <= 30; i++) {
             new Thread(() -> {
                 list.add(UUID.randomUUID().toString().substring(0, 6));
